7f4e437637e87ab1c057fb287af1496b5283f6db
xan187 <cloud901memphis@gmail.com>
2024-10-18 03:58:11 -0500

    Initialize snyk-fix-05712b105cfbd8f5605be86d85420bd1

---
+++ packages/connect/src/index.ts
@@ -0,0 +1,19 @@
+export { HttpClient } from './http_client';
+export { ordersChannelFactory } from './orders_channel_factory';
+export { OrdersChannel, OrdersChannelHandler } from './types';
+export {
+    APIOrder,
+    AssetPairsRequestOpts,
+    AssetPairsResponse,
+    FeeRecipientsResponse,
+    OrderbookRequest,
+    OrderbookResponse,
+    OrderConfigRequest,
+    OrderConfigResponse,
+    OrdersChannelSubscriptionOpts,
+    OrdersRequestOpts,
+    OrdersResponse,
+    PagedRequestOpts,
+    PaginatedCollection,
+    SignedOrder,
+} from '@0x/types';
