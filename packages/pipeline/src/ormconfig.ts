cdb24f9cae215182974e56fa67b899444eb81d06
xan187 <cloud901memphis@gmail.com>
2024-10-18 02:49:38 -0500

    Initialize greenkeeper/lerna-3.13.1

---
+++ packages/pipeline/src/ormconfig.ts
@@ -0,0 +1,58 @@
+import { ConnectionOptions } from 'typeorm';
+
+import {
+    Block,
+    CopperActivity,
+    CopperActivityType,
+    CopperCustomField,
+    CopperLead,
+    CopperOpportunity,
+    DexTrade,
+    ERC20ApprovalEvent,
+    ExchangeCancelEvent,
+    ExchangeCancelUpToEvent,
+    ExchangeFillEvent,
+    NonfungibleDotComTrade,
+    OHLCVExternal,
+    Relayer,
+    Slippage,
+    SraOrder,
+    SraOrdersObservedTimeStamp,
+    TokenMetadata,
+    TokenOrderbookSnapshot,
+    Transaction,
+} from './entities';
+
+const entities = [
+    Block,
+    CopperOpportunity,
+    CopperActivity,
+    CopperActivityType,
+    CopperCustomField,
+    CopperLead,
+    DexTrade,
+    ExchangeCancelEvent,
+    ExchangeCancelUpToEvent,
+    ExchangeFillEvent,
+    ERC20ApprovalEvent,
+    NonfungibleDotComTrade,
+    OHLCVExternal,
+    Relayer,
+    Slippage,
+    SraOrder,
+    SraOrdersObservedTimeStamp,
+    TokenMetadata,
+    TokenOrderbookSnapshot,
+    Transaction,
+];
+
+const config: ConnectionOptions = {
+    type: 'postgres',
+    url: process.env.ZEROEX_DATA_PIPELINE_DB_URL,
+    synchronize: false,
+    logging: ['error'],
+    entities,
+    migrations: ['./lib/migrations/**/*.js'],
+};
+
+module.exports = config;
