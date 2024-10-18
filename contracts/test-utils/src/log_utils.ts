1e9ce85bbec1f425a4a3c4f988a7a1ea29ea3703
xan187 <cloud901memphis@gmail.com>
2024-10-18 03:14:38 -0500

    Initialize fix/3.0-audit/staking/assert-valid-params-in-MixinParams

---
+++ contracts/test-utils/src/log_utils.ts
@@ -0,0 +1,34 @@
+import { LogEntry, LogWithDecodedArgs, TransactionReceiptWithDecodedLogs } from 'ethereum-types';
+
+import { expect } from './chai_setup';
+
+// tslint:disable no-unnecessary-type-assertion
+
+/**
+ * Filter logs by event name/type.
+ */
+export function filterLogs<TEventArgs>(logs: LogEntry[], event: string): Array<LogWithDecodedArgs<TEventArgs>> {
+    return (logs as Array<LogWithDecodedArgs<any>>).filter(log => log.event === event);
+}
+
+/**
+ * Filter logs by event name/type and convert to arguments.
+ */
+export function filterLogsToArguments<TEventArgs>(logs: LogEntry[], event: string): TEventArgs[] {
+    return filterLogs<TEventArgs>(logs, event).map(log => log.args);
+}
+
+/**
+ * Verifies that a transaction emitted the expected events of a particular type.
+ */
+export function verifyEvents<TEventArgs>(
+    txReceipt: TransactionReceiptWithDecodedLogs,
+    expectedEvents: TEventArgs[],
+    eventName: string,
+): void {
+    const logs = filterLogsToArguments<TEventArgs>(txReceipt.logs, eventName);
+    expect(logs.length).to.eq(expectedEvents.length);
+    logs.forEach((log, index) => {
+        expect(log).to.deep.equal(expectedEvents[index]);
+    });
+}
