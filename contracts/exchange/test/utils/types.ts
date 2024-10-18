ba19cd29d1e833cdb011933152b83f9ff0ead7dc
xan187 <cloud901memphis@gmail.com>
2024-10-18 03:06:06 -0500

    Initialize feature/staking/NewMechanicsSolidityOnly-Squashed-Tests-Squashed

---
+++ contracts/exchange/test/utils/types.ts
@@ -0,0 +1,37 @@
+import { SignedOrder } from '@0x/types';
+import { BigNumber } from '@0x/utils';
+
+export interface AbiDecodedFillOrderData {
+    order: SignedOrder;
+    takerAssetFillAmount: BigNumber;
+    signature: string;
+}
+
+export enum ExchangeFunctionName {
+    BatchCancelOrders = 'batchCancelOrders',
+    BatchExecuteTransactions = 'batchExecuteTransactions',
+    BatchFillOrKillOrders = 'batchFillOrKillOrders',
+    BatchFillOrders = 'batchFillOrders',
+    BatchFillOrdersNoThrow = 'batchFillOrdersNoThrow',
+    BatchMatchOrders = 'batchMatchOrders',
+    BatchMatchordersWithMaximalFill = 'batchMatchOrdersWithMaximalFill',
+    CancelOrder = 'cancelOrder',
+    CancelOrdersUpTo = 'cancelOrdersUpTo',
+    ExecuteTransaction = 'executeTransaction',
+    FillOrKillOrder = 'fillOrKillOrder',
+    FillOrder = 'fillOrder',
+    FillOrderNoThrow = 'fillOrderNoThrow',
+    MarketBuyOrdersNoThrow = 'marketBuyOrdersNoThrow',
+    MarketSellOrdersNoThrow = 'marketSellOrdersNoThrow',
+    MarketBuyOrdersFillOrKill = 'marketBuyOrdersFillOrKill',
+    MarketSellOrdersFillOrKill = 'marketSellOrdersFillOrKill',
+    MatchOrders = 'matchOrders',
+    MatchOrdersWithMaximalFill = 'matchOrdersWithMaximalFill',
+    PreSign = 'preSign',
+    RegisterAssetProxy = 'registerAssetProxy',
+    SetSignatureValidatorApproval = 'setSignatureValidatorApproval',
+    SimulateDispatchTransferFromCalls = 'simulateDispatchTransferFromCalls',
+    TransferOwnership = 'transferOwnership',
+    SetProtocolFeeMultiplier = 'setProtocolFeeMultiplier',
+    SetProtocolFeeCollectorAddress = 'setProtocolFeeCollectorAddress',
+}
