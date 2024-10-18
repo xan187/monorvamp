c11d82376f88d5ebb5f8c3b523ba5ccd56e903ad
xan187 <cloud901memphis@gmail.com>
2024-10-18 03:25:46 -0500

    Initialize feature/erc20-bridge-sampler/e2e-plp-integration

---
+++ packages/asset-swapper/src/utils/market_operation_utils/constants.ts
@@ -0,0 +1,45 @@
+import { BigNumber } from '@0x/utils';
+
+import { ERC20BridgeSource, GetMarketOrdersOpts, StandardERC20BridgeSourceMapping } from './types';
+
+const INFINITE_TIMESTAMP_SEC = new BigNumber(2524604400);
+
+/**
+ * Valid sources for market sell.
+ */
+export const SELL_MAPPINGS: StandardERC20BridgeSourceMapping[] = [
+    { source: ERC20BridgeSource.Uniswap },
+    { source: ERC20BridgeSource.Eth2Dai },
+    { source: ERC20BridgeSource.Kyber },
+    { source: ERC20BridgeSource.CurveUsdcDai },
+    { source: ERC20BridgeSource.CurveUsdcDaiUsdt },
+    { source: ERC20BridgeSource.CurveUsdcDaiUsdtTusd },
+];
+
+/**
+ * Valid sources for market buy.
+ */
+export const BUY_MAPPINGS: StandardERC20BridgeSourceMapping[] = [
+    { source: ERC20BridgeSource.Uniswap },
+    { source: ERC20BridgeSource.Eth2Dai },
+];
+
+export const DEFAULT_GET_MARKET_ORDERS_OPTS: GetMarketOrdersOpts = {
+    // tslint:disable-next-line: custom-no-magic-numbers
+    runLimit: 2 ** 15,
+    excludedSources: [],
+    bridgeSlippage: 0.0005,
+    dustFractionThreshold: 0.0025,
+    numSamples: 13,
+    noConflicts: true,
+    sampleDistributionBase: 1.05,
+};
+
+export const constants = {
+    INFINITE_TIMESTAMP_SEC,
+    SELL_MAPPINGS,
+    BUY_MAPPINGS,
+    DEFAULT_GET_MARKET_ORDERS_OPTS,
+    ERC20_PROXY_ID: '0xf47261b0',
+    WALLET_SIGNATURE: '0x04',
+};
