6d78a08aa7698af00c2e610acf44101ea980919b
xan187 <cloud901memphis@gmail.com>
2024-10-18 02:40:13 -0500

    Initialize dependabot/npm_and_yarn/sleep-5.2.3

---
+++ packages/subproviders/src/subproviders/subprovider.ts
@@ -0,0 +1,62 @@
+import { promisify } from '@0xproject/utils';
+import { JSONRPCRequestPayload, JSONRPCResponsePayload, Provider } from 'ethereum-types';
+
+import { Callback, ErrorCallback, JSONRPCRequestPayloadWithMethod } from '../types';
+/**
+ * A altered version of the base class Subprovider found in [web3-provider-engine](https://github.com/MetaMask/provider-engine).
+ * This one has an async/await `emitPayloadAsync` and also defined types.
+ */
+export abstract class Subprovider {
+    // tslint:disable-next-line:underscore-private-and-protected
+    private engine!: Provider;
+    protected static _createFinalPayload(
+        payload: Partial<JSONRPCRequestPayloadWithMethod>,
+    ): Partial<JSONRPCRequestPayloadWithMethod> {
+        const finalPayload = {
+            // defaults
+            id: Subprovider._getRandomId(),
+            jsonrpc: '2.0',
+            params: [],
+            ...payload,
+        };
+        return finalPayload;
+    }
+    // Ported from: https://github.com/MetaMask/provider-engine/blob/master/util/random-id.js
+    private static _getRandomId(): number {
+        const extraDigits = 3;
+        const baseTen = 10;
+        // 13 time digits
+        const datePart = new Date().getTime() * Math.pow(baseTen, extraDigits);
+        // 3 random digits
+        const extraPart = Math.floor(Math.random() * Math.pow(baseTen, extraDigits));
+        // 16 digits
+        return datePart + extraPart;
+    }
+    // tslint:disable-next-line:async-suffix
+    public abstract async handleRequest(
+        payload: JSONRPCRequestPayload,
+        next: Callback,
+        end: ErrorCallback,
+    ): Promise<void>;
+
+    /**
+     * Emits a JSON RPC payload that will then be handled by the ProviderEngine instance
+     * this subprovider is a part of. The payload will cascade down the subprovider middleware
+     * stack until finding the responsible entity for handling the request.
+     * @param payload JSON RPC payload
+     * @returns JSON RPC response payload
+     */
+    public async emitPayloadAsync(payload: Partial<JSONRPCRequestPayloadWithMethod>): Promise<JSONRPCResponsePayload> {
+        const finalPayload = Subprovider._createFinalPayload(payload);
+        const response = await promisify<JSONRPCResponsePayload>(this.engine.sendAsync, this.engine)(finalPayload);
+        return response;
+    }
+    /**
+     * Set's the subprovider's engine to the ProviderEngine it is added to.
+     * This is only called within the ProviderEngine source code, do not call
+     * directly.
+     */
+    public setEngine(engine: Provider): void {
+        this.engine = engine;
+    }
+}
