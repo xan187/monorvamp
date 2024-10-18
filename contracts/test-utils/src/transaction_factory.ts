9a18d0d3907b31a68b79471245ef87005306d69a
xan187 <cloud901memphis@gmail.com>
2024-10-18 02:52:36 -0500

    Initialize feature/3.0/exchange/order-validator-signature

---
+++ contracts/test-utils/src/transaction_factory.ts
@@ -0,0 +1,43 @@
+import { generatePseudoRandomSalt, transactionHashUtils } from '@0x/order-utils';
+import { SignatureType, SignedZeroExTransaction } from '@0x/types';
+import * as ethUtil from 'ethereumjs-util';
+
+import { signingUtils } from './signing_utils';
+
+export class TransactionFactory {
+    private readonly _signerBuff: Buffer;
+    private readonly _exchangeAddress: string;
+    private readonly _privateKey: Buffer;
+    private readonly _chainId: number;
+
+    constructor(privateKey: Buffer, exchangeAddress: string, chainId: number) {
+        this._privateKey = privateKey;
+        this._exchangeAddress = exchangeAddress;
+        this._chainId = chainId;
+        this._signerBuff = ethUtil.privateToAddress(this._privateKey);
+    }
+    public newSignedTransaction(
+        data: string,
+        signatureType: SignatureType = SignatureType.EthSign,
+    ): SignedZeroExTransaction {
+        const salt = generatePseudoRandomSalt();
+        const signerAddress = `0x${this._signerBuff.toString('hex')}`;
+        const transaction = {
+            salt,
+            signerAddress,
+            data,
+            domain: {
+                verifyingContractAddress: this._exchangeAddress,
+                chainId: this._chainId,
+            },
+        };
+
+        const transactionHashBuffer = transactionHashUtils.getTransactionHashBuffer(transaction);
+        const signature = signingUtils.signMessage(transactionHashBuffer, this._privateKey, signatureType);
+        const signedTransaction = {
+            ...transaction,
+            signature: `0x${signature.toString('hex')}`,
+        };
+        return signedTransaction;
+    }
+}
---
+++ contracts/utils/contracts/src/ReentrancyGuard.sol
@@ -0,0 +1,41 @@
+/*
+
+  Copyright 2018 ZeroEx Intl.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+
+pragma solidity ^0.5.5;
+
+
+contract ReentrancyGuard {
+
+    // Mutex counter.
+    // Starts at 1 and increases whenever a nonReentrant function is called.
+    uint256 private reentrancyGuardCounter = 1;
+
+    /// @dev Functions with this modifer cannot be reentered.
+    modifier nonReentrant() {
+        // Increment and remember the current counter value.
+        uint256 localCounter = ++reentrancyGuardCounter;
+        // Call the function.
+        _;
+        // If the counter value is different from what we remember,
+        // the function was called more than once.
+        require(
+            localCounter == reentrancyGuardCounter,
+            "REENTRANCY_ILLEGAL"
+        );
+    }
+}
