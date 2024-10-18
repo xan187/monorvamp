417bdd076c8be5dd454d849c577787864d16b3b4
xan187 <cloud901memphis@gmail.com>
2024-10-18 03:27:51 -0500

    Initialize fix/py-mesh-ganache-integration

---
+++ packages/asset-swapper/test/utils/swap_quote.ts
@@ -0,0 +1,58 @@
+import { BigNumber } from '@0x/utils';
+import * as _ from 'lodash';
+
+import { ERC20BridgeSource } from '../../src';
+import { constants } from '../../src/constants';
+import { MarketOperation, SignedOrderWithFillableAmounts, SwapQuote } from '../../src/types';
+import { ProtocolFeeUtils } from '../../src/utils/protocol_fee_utils';
+
+/**
+ * Creates a swap quote given orders.
+ */
+export async function getFullyFillableSwapQuoteWithNoFeesAsync(
+    makerAssetData: string,
+    takerAssetData: string,
+    orders: SignedOrderWithFillableAmounts[],
+    operation: MarketOperation,
+    gasPrice: BigNumber,
+    protocolFeeUtils: ProtocolFeeUtils,
+): Promise<SwapQuote> {
+    const makerAssetFillAmount = BigNumber.sum(...[0, ...orders.map(o => o.makerAssetAmount)]);
+    const totalTakerAssetAmount = BigNumber.sum(...[0, ...orders.map(o => o.takerAssetAmount)]);
+    const quoteInfo = {
+        makerAssetAmount: makerAssetFillAmount,
+        feeTakerAssetAmount: constants.ZERO_AMOUNT,
+        takerAssetAmount: totalTakerAssetAmount,
+        totalTakerAssetAmount,
+        protocolFeeInWeiAmount: await protocolFeeUtils.calculateWorstCaseProtocolFeeAsync(orders, gasPrice),
+        gas: 200e3,
+    };
+
+    const breakdown = {
+        [ERC20BridgeSource.Native]: new BigNumber(1),
+    };
+
+    const quoteBase = {
+        makerAssetData,
+        takerAssetData,
+        orders,
+        gasPrice,
+        bestCaseQuoteInfo: quoteInfo,
+        worstCaseQuoteInfo: quoteInfo,
+        sourceBreakdown: breakdown,
+    };
+
+    if (operation === MarketOperation.Buy) {
+        return {
+            ...quoteBase,
+            type: MarketOperation.Buy,
+            makerAssetFillAmount,
+        };
+    } else {
+        return {
+            ...quoteBase,
+            type: MarketOperation.Sell,
+            takerAssetFillAmount: totalTakerAssetAmount,
+        };
+    }
+}
