61ee00fc994bd579e06d2fcc25d43ffdadb8dab1
xan187 <cloud901memphis@gmail.com>
2024-10-18 03:15:45 -0500

    Initialize contract-addresses-in-json

---
+++ packages/contract-addresses/src/index.ts
@@ -0,0 +1,53 @@
+import * as fs from 'fs';
+import * as _ from 'lodash';
+
+export interface ContractAddresses {
+    erc20Proxy: string;
+    erc721Proxy: string;
+    zrxToken: string;
+    etherToken: string;
+    exchangeV2: string;
+    exchange: string;
+    assetProxyOwner: string;
+    zeroExGovernor: string;
+    forwarder: string;
+    orderValidator: string;
+    dutchAuction: string;
+    coordinatorRegistry: string;
+    coordinator: string;
+    multiAssetProxy: string;
+    staticCallProxy: string;
+    erc1155Proxy: string;
+    devUtils: string;
+    zrxVault: string;
+    staking: string;
+    stakingProxy: string;
+    erc20BridgeProxy: string;
+}
+
+export enum NetworkId {
+    Mainnet = 1,
+    Ropsten = 3,
+    Rinkeby = 4,
+    Kovan = 42,
+    Ganache = 50,
+}
+
+/**
+ * Used to get addresses of contracts that have been deployed to either the
+ * Ethereum mainnet or a supported testnet. Throws if there are no known
+ * contracts deployed on the corresponding network.
+ * @param networkId The desired networkId.
+ * @returns The set of addresses for contracts which have been deployed on the
+ * given networkId.
+ */
+export function getContractAddressesForNetworkOrThrow(networkId: NetworkId): ContractAddresses {
+    const networkToAddresses: { [networkId: number]: ContractAddresses } = JSON.parse(
+        fs.readFileSync('addresses.json').toString(),
+    );
+
+    if (networkToAddresses[networkId] === undefined) {
+        throw new Error(`Unknown network id (${networkId}). No known 0x contracts have been deployed on this network.`);
+    }
+    return networkToAddresses[networkId];
+}
