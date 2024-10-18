2df8bcfcf9ffa0c810861b841339074d73aef3f0
xan187 <cloud901memphis@gmail.com>
2024-10-18 02:58:11 -0500

    Initialize feature/3.0/exchange/always-validate-order-signatures

---
+++ contracts/exchange/test/utils/types.ts
@@ -0,0 +1,27 @@
+import { SignedOrder } from '@0x/types';
+import { BigNumber } from '@0x/utils';
+
+export interface AbiDecodedFillOrderData {
+    order: SignedOrder;
+    takerAssetFillAmount: BigNumber;
+    signature: string;
+}
+
+export enum ExchangeFunctionName {
+    FillOrder = 'fillOrder',
+    FillOrKillOrder = 'fillOrKillOrder',
+    FillOrderNoThrow = 'fillOrderNoThrow',
+    BatchFillOrders = 'batchFillOrders',
+    BatchFillOrKillOrders = 'batchFillOrKillOrders',
+    BatchFillOrdersNoThrow = 'batchFillOrdersNoThrow',
+    MarketBuyOrders = 'marketBuyOrders',
+    MarketBuyOrdersNoThrow = 'marketBuyOrdersNoThrow',
+    MarketSellOrders = 'marketSellOrders',
+    MarketSellOrdersNoThrow = 'marketSellOrdersNoThrow',
+    MatchOrders = 'matchOrders',
+    CancelOrder = 'cancelOrder',
+    BatchCancelOrders = 'batchCancelOrders',
+    CancelOrdersUpTo = 'cancelOrdersUpTo',
+    PreSign = 'preSign',
+    SetSignatureValidatorApproval = 'setSignatureValidatorApproval',
+}
