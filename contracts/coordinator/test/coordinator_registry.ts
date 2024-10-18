5f92b3a4722183e76d7cb78e05a1f753f51a0419
xan187 <cloud901memphis@gmail.com>
2024-10-18 03:14:03 -0500

    Initialize refactor/balance-stores

---
+++ contracts/coordinator/test/coordinator_registry.ts
@@ -0,0 +1,77 @@
+import { blockchainTests, expect } from '@0x/contracts-test-utils';
+
+import {
+    artifacts,
+    CoordinatorRegistryContract,
+    CoordinatorRegistryCoordinatorEndpointSetEventArgs,
+    CoordinatorTestFactory,
+} from '../src';
+
+// tslint:disable:no-unnecessary-type-assertion
+blockchainTests.resets('Coordinator Registry tests', env => {
+    let coordinatorRegistry: CoordinatorRegistryContract;
+    let coordinatorOperator: string;
+    const coordinatorEndpoint = 'http://sometec.0x.org';
+    const nilCoordinatorEndpoint = '';
+    // tests
+    before(async () => {
+        // setup accounts (skip owner)
+        const accounts = await env.getAccountAddressesAsync();
+        [, coordinatorOperator] = accounts;
+        // deploy coordinator registry
+        coordinatorRegistry = await CoordinatorRegistryContract.deployFrom0xArtifactAsync(
+            artifacts.CoordinatorRegistry,
+            env.provider,
+            env.txDefaults,
+            artifacts,
+        );
+    });
+    describe('core', () => {
+        it('Should successfully set a Coordinator endpoint', async () => {
+            await coordinatorRegistry.setCoordinatorEndpoint.awaitTransactionSuccessAsync(coordinatorEndpoint, {
+                from: coordinatorOperator,
+            });
+            const recordedCoordinatorEndpoint = await coordinatorRegistry.getCoordinatorEndpoint.callAsync(
+                coordinatorOperator,
+            );
+            expect(recordedCoordinatorEndpoint).to.be.equal(coordinatorEndpoint);
+        });
+        it('Should successfully unset a Coordinator endpoint', async () => {
+            // set Coordinator endpoint
+            await coordinatorRegistry.setCoordinatorEndpoint.awaitTransactionSuccessAsync(coordinatorEndpoint, {
+                from: coordinatorOperator,
+            });
+            let recordedCoordinatorEndpoint = await coordinatorRegistry.getCoordinatorEndpoint.callAsync(
+                coordinatorOperator,
+            );
+            expect(recordedCoordinatorEndpoint).to.be.equal(coordinatorEndpoint);
+            // unset Coordinator endpoint
+            await coordinatorRegistry.setCoordinatorEndpoint.awaitTransactionSuccessAsync(nilCoordinatorEndpoint, {
+                from: coordinatorOperator,
+            });
+            recordedCoordinatorEndpoint = await coordinatorRegistry.getCoordinatorEndpoint.callAsync(
+                coordinatorOperator,
+            );
+            expect(recordedCoordinatorEndpoint).to.be.equal(nilCoordinatorEndpoint);
+        });
+        it('Should emit an event when setting Coordinator endpoint', async () => {
+            // set Coordinator endpoint
+            const txReceipt = await coordinatorRegistry.setCoordinatorEndpoint.awaitTransactionSuccessAsync(
+                coordinatorEndpoint,
+                {
+                    from: coordinatorOperator,
+                },
+            );
+            const recordedCoordinatorEndpoint = await coordinatorRegistry.getCoordinatorEndpoint.callAsync(
+                coordinatorOperator,
+            );
+            expect(recordedCoordinatorEndpoint).to.be.equal(coordinatorEndpoint);
+            // validate event
+            const expectedEvent: CoordinatorRegistryCoordinatorEndpointSetEventArgs = {
+                coordinatorOperator,
+                coordinatorEndpoint,
+            };
+            CoordinatorTestFactory.verifyEvents(txReceipt, [expectedEvent], 'CoordinatorEndpointSet');
+        });
+    });
+});
