1d6859e1aa69a156c792ace5322fa47127228c2a
xan187 <cloud901memphis@gmail.com>
2024-10-18 03:11:24 -0500

    Initialize feature/orderbook-removing-smart-routing-logic

---
+++ contracts/staking/contracts/src/sys/MixinParams.sol
@@ -0,0 +1,327 @@
+/*
+
+  Copyright 2019 ZeroEx Intl.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+
+pragma solidity ^0.5.9;
+
+import "@0x/contracts-utils/contracts/src/LibRichErrors.sol";
+import "@0x/contracts-asset-proxy/contracts/src/interfaces/IAssetProxy.sol";
+import "../immutable/MixinStorage.sol";
+import "../interfaces/IStakingEvents.sol";
+import "../interfaces/IEthVault.sol";
+import "../interfaces/IStakingPoolRewardVault.sol";
+import "../interfaces/IZrxVault.sol";
+import "../libs/LibStakingRichErrors.sol";
+
+
+contract MixinParams is
+    IStakingEvents,
+    MixinStorage
+{
+    /// @dev Set all configurable parameters at once.
+    /// @param _epochDurationInSeconds Minimum seconds between epochs.
+    /// @param _rewardDelegatedStakeWeight How much delegated stake is weighted vs operator stake, in ppm.
+    /// @param _minimumPoolStake Minimum amount of stake required in a pool to collect rewards.
+    /// @param _maximumMakersInPool Maximum number of maker addresses allowed to be registered to a pool.
+    /// @param _cobbDouglasAlphaNumerator Numerator for cobb douglas alpha factor.
+    /// @param _cobbDouglasAlphaDenominator Denominator for cobb douglas alpha factor.
+    /// @param _wethProxyAddress The address that can transfer WETH for fees.
+    /// @param _ethVaultAddress Address of the EthVault contract.
+    /// @param _rewardVaultAddress Address of the StakingPoolRewardVault contract.
+    /// @param _zrxVaultAddress Address of the ZrxVault contract.
+    function setParams(
+        uint256 _epochDurationInSeconds,
+        uint32 _rewardDelegatedStakeWeight,
+        uint256 _minimumPoolStake,
+        uint256 _maximumMakersInPool,
+        uint32 _cobbDouglasAlphaNumerator,
+        uint32 _cobbDouglasAlphaDenominator,
+        address _wethProxyAddress,
+        address _ethVaultAddress,
+        address payable _rewardVaultAddress,
+        address _zrxVaultAddress
+    )
+        external
+        onlyOwner
+    {
+        _setParams(
+            _epochDurationInSeconds,
+            _rewardDelegatedStakeWeight,
+            _minimumPoolStake,
+            _maximumMakersInPool,
+            _cobbDouglasAlphaNumerator,
+            _cobbDouglasAlphaDenominator,
+            _wethProxyAddress,
+            _ethVaultAddress,
+            _rewardVaultAddress,
+            _zrxVaultAddress
+        );
+    }
+
+    /// @dev Retrieves all configurable parameter values.
+    /// @return _epochDurationInSeconds Minimum seconds between epochs.
+    /// @return _rewardDelegatedStakeWeight How much delegated stake is weighted vs operator stake, in ppm.
+    /// @return _minimumPoolStake Minimum amount of stake required in a pool to collect rewards.
+    /// @return _maximumMakersInPool Maximum number of maker addresses allowed to be registered to a pool.
+    /// @return _cobbDouglasAlphaNumerator Numerator for cobb douglas alpha factor.
+    /// @return _cobbDouglasAlphaDenominator Denominator for cobb douglas alpha factor.
+    /// @return _wethProxyAddress The address that can transfer WETH for fees.
+    /// @return _ethVaultAddress Address of the EthVault contract.
+    /// @return _rewardVaultAddress Address of the StakingPoolRewardVault contract.
+    /// @return _zrxVaultAddress Address of the ZrxVault contract.
+    function getParams()
+        external
+        view
+        returns (
+            uint256 _epochDurationInSeconds,
+            uint32 _rewardDelegatedStakeWeight,
+            uint256 _minimumPoolStake,
+            uint256 _maximumMakersInPool,
+            uint32 _cobbDouglasAlphaNumerator,
+            uint32 _cobbDouglasAlphaDenominator,
+            address _wethProxyAddress,
+            address _ethVaultAddress,
+            address _rewardVaultAddress,
+            address _zrxVaultAddress
+        )
+    {
+        _epochDurationInSeconds = epochDurationInSeconds;
+        _rewardDelegatedStakeWeight = rewardDelegatedStakeWeight;
+        _minimumPoolStake = minimumPoolStake;
+        _maximumMakersInPool = maximumMakersInPool;
+        _cobbDouglasAlphaNumerator = cobbDouglasAlphaNumerator;
+        _cobbDouglasAlphaDenominator = cobbDouglasAlphaDenominator;
+        _wethProxyAddress = address(wethAssetProxy);
+        _ethVaultAddress = address(ethVault);
+        _rewardVaultAddress = address(rewardVault);
+        _zrxVaultAddress = address(zrxVault);
+    }
+
+    /// @dev Initialize storage belonging to this mixin.
+    /// @param _wethProxyAddress The address that can transfer WETH for fees.
+    /// @param _ethVaultAddress Address of the EthVault contract.
+    /// @param _rewardVaultAddress Address of the StakingPoolRewardVault contract.
+    /// @param _zrxVaultAddress Address of the ZrxVault contract.
+    function _initMixinParams(
+        address _wethProxyAddress,
+        address _ethVaultAddress,
+        address payable _rewardVaultAddress,
+        address _zrxVaultAddress
+    )
+        internal
+    {
+        // Ensure state is uninitialized.
+        _assertParamsNotInitialized();
+
+        // Set up defaults.
+        // These cannot be set to variables, or we go over the stack variable limit.
+        _setParams(
+            2 weeks,                       // epochDurationInSeconds
+            (90 * PPM_DENOMINATOR) / 100,  // rewardDelegatedStakeWeight
+            100 * MIN_TOKEN_VALUE,         // minimumPoolStake
+            10,                            // maximumMakersInPool
+            1,                             // cobbDouglasAlphaNumerator
+            2,                             // cobbDouglasAlphaDenominator
+            _wethProxyAddress,
+            _ethVaultAddress,
+            _rewardVaultAddress,
+            _zrxVaultAddress
+        );
+    }
+
+    /// @dev Asserts that upgradable storage has not yet been initialized.
+    function _assertParamsNotInitialized()
+        internal
+        view
+    {
+        if (epochDurationInSeconds != 0 &&
+            rewardDelegatedStakeWeight != 0 &&
+            minimumPoolStake != 0 &&
+            maximumMakersInPool != 0 &&
+            cobbDouglasAlphaNumerator != 0 &&
+            cobbDouglasAlphaDenominator != 0 &&
+            address(wethAssetProxy) != NIL_ADDRESS &&
+            address(ethVault) != NIL_ADDRESS &&
+            address(rewardVault) != NIL_ADDRESS &&
+            address(zrxVault) != NIL_ADDRESS
+        ) {
+            LibRichErrors.rrevert(
+                LibStakingRichErrors.InitializationError(
+                    LibStakingRichErrors.InitializationErrorCode.MixinParamsAlreadyInitialized
+                )
+            );
+        }
+    }
+
+    /// @dev Set all configurable parameters at once.
+    /// @param _epochDurationInSeconds Minimum seconds between epochs.
+    /// @param _rewardDelegatedStakeWeight How much delegated stake is weighted vs operator stake, in ppm.
+    /// @param _minimumPoolStake Minimum amount of stake required in a pool to collect rewards.
+    /// @param _maximumMakersInPool Maximum number of maker addresses allowed to be registered to a pool.
+    /// @param _cobbDouglasAlphaNumerator Numerator for cobb douglas alpha factor.
+    /// @param _cobbDouglasAlphaDenominator Denominator for cobb douglas alpha factor.
+    /// @param _wethProxyAddress The address that can transfer WETH for fees.
+    /// @param _ethVaultAddress Address of the EthVault contract.
+    /// @param _rewardVaultAddress Address of the StakingPoolRewardVault contract.
+    /// @param _zrxVaultAddress Address of the ZrxVault contract.
+    function _setParams(
+        uint256 _epochDurationInSeconds,
+        uint32 _rewardDelegatedStakeWeight,
+        uint256 _minimumPoolStake,
+        uint256 _maximumMakersInPool,
+        uint32 _cobbDouglasAlphaNumerator,
+        uint32 _cobbDouglasAlphaDenominator,
+        address _wethProxyAddress,
+        address _ethVaultAddress,
+        address payable _rewardVaultAddress,
+        address _zrxVaultAddress
+    )
+        private
+    {
+        _assertValidRewardDelegatedStakeWeight(_rewardDelegatedStakeWeight);
+        _assertValidMaximumMakersInPool(_maximumMakersInPool);
+        _assertValidCobbDouglasAlpha(
+            _cobbDouglasAlphaNumerator,
+            _cobbDouglasAlphaDenominator
+        );
+        _assertValidAddresses(
+            _wethProxyAddress,
+            _ethVaultAddress,
+            _rewardVaultAddress,
+            _zrxVaultAddress
+        );
+        // TODO: set boundaries on some of these params
+
+        epochDurationInSeconds = _epochDurationInSeconds;
+        rewardDelegatedStakeWeight = _rewardDelegatedStakeWeight;
+        minimumPoolStake = _minimumPoolStake;
+        maximumMakersInPool = _maximumMakersInPool;
+        cobbDouglasAlphaNumerator = _cobbDouglasAlphaNumerator;
+        cobbDouglasAlphaDenominator = _cobbDouglasAlphaDenominator;
+        wethAssetProxy = IAssetProxy(_wethProxyAddress);
+        ethVault = IEthVault(_ethVaultAddress);
+        rewardVault = IStakingPoolRewardVault(_rewardVaultAddress);
+        zrxVault = IZrxVault(_zrxVaultAddress);
+
+        emit ParamsSet(
+            _epochDurationInSeconds,
+            _rewardDelegatedStakeWeight,
+            _minimumPoolStake,
+            _maximumMakersInPool,
+            _cobbDouglasAlphaNumerator,
+            _cobbDouglasAlphaDenominator,
+            _wethProxyAddress,
+            _ethVaultAddress,
+            _rewardVaultAddress,
+            _zrxVaultAddress
+        );
+    }
+
+    /// @dev Asserts that cobb douglas alpha values are valid.
+    /// @param numerator Numerator for cobb douglas alpha factor.
+    /// @param denominator Denominator for cobb douglas alpha factor.
+    function _assertValidCobbDouglasAlpha(
+        uint32 numerator,
+        uint32 denominator
+    )
+        private
+        pure
+    {
+        // Alpha must be 0 < x < 1
+        if (numerator > denominator || denominator == 0) {
+            LibRichErrors.rrevert(
+                LibStakingRichErrors.InvalidParamValueError(
+                    LibStakingRichErrors.InvalidParamValueErrorCode.InvalidCobbDouglasAlpha
+            ));
+        }
+    }
+
+    /// @dev Asserts that a stake weight is valid.
+    /// @param weight How much delegated stake is weighted vs operator stake, in ppm.
+    function _assertValidRewardDelegatedStakeWeight(
+        uint32 weight
+    )
+        private
+        pure
+    {
+        if (weight > PPM_DENOMINATOR) {
+            LibRichErrors.rrevert(
+                LibStakingRichErrors.InvalidParamValueError(
+                    LibStakingRichErrors.InvalidParamValueErrorCode.InvalidRewardDelegatedStakeWeight
+            ));
+        }
+    }
+
+    /// @dev Asserts that a maximum makers value is valid.
+    /// @param amount Maximum number of maker addresses allowed to be registered to a pool.
+    function _assertValidMaximumMakersInPool(
+        uint256 amount
+    )
+        private
+        pure
+    {
+        if (amount == 0) {
+            LibRichErrors.rrevert(
+                LibStakingRichErrors.InvalidParamValueError(
+                    LibStakingRichErrors.InvalidParamValueErrorCode.InvalidMaximumMakersInPool
+            ));
+        }
+    }
+
+    /// @dev Asserts that passed in addresses are non-zero.
+    /// @param _wethProxyAddress The address that can transfer WETH for fees.
+    /// @param _ethVaultAddress Address of the EthVault contract.
+    /// @param _rewardVaultAddress Address of the StakingPoolRewardVault contract.
+    /// @param _zrxVaultAddress Address of the ZrxVault contract.
+    function _assertValidAddresses(
+        address _wethProxyAddress,
+        address _ethVaultAddress,
+        address _rewardVaultAddress,
+        address _zrxVaultAddress
+    )
+        private
+        pure
+    {
+        if (_wethProxyAddress == NIL_ADDRESS) {
+            LibRichErrors.rrevert(
+                LibStakingRichErrors.InvalidParamValueError(
+                    LibStakingRichErrors.InvalidParamValueErrorCode.InvalidWethProxyAddress
+            ));
+        }
+
+        if (_ethVaultAddress == NIL_ADDRESS) {
+            LibRichErrors.rrevert(
+                LibStakingRichErrors.InvalidParamValueError(
+                    LibStakingRichErrors.InvalidParamValueErrorCode.InvalidEthVaultAddress
+            ));
+        }
+
+        if (_rewardVaultAddress == NIL_ADDRESS) {
+            LibRichErrors.rrevert(
+                LibStakingRichErrors.InvalidParamValueError(
+                    LibStakingRichErrors.InvalidParamValueErrorCode.InvalidRewardVaultAddress
+            ));
+        }
+
+        if (_zrxVaultAddress == NIL_ADDRESS) {
+            LibRichErrors.rrevert(
+                LibStakingRichErrors.InvalidParamValueError(
+                    LibStakingRichErrors.InvalidParamValueErrorCode.InvalidZrxVaultAddress
+            ));
+        }
+    }
+}
---
+++ contracts/staking/test/params.ts
@@ -0,0 +1,158 @@
+import { blockchainTests, constants, expect, filterLogsToArguments } from '@0x/contracts-test-utils';
+import { StakingRevertErrors } from '@0x/order-utils';
+import { BigNumber, OwnableRevertErrors } from '@0x/utils';
+
+import { artifacts, IStakingEventsParamsSetEventArgs, MixinParamsContract } from '../src/';
+
+import { constants as stakingConstants } from './utils/constants';
+import { StakingParams } from './utils/types';
+
+blockchainTests('Configurable Parameters', env => {
+    let testContract: MixinParamsContract;
+    let ownerAddress: string;
+    let notOwnerAddress: string;
+
+    before(async () => {
+        [ownerAddress, notOwnerAddress] = await env.getAccountAddressesAsync();
+        testContract = await MixinParamsContract.deployFrom0xArtifactAsync(
+            artifacts.MixinParams,
+            env.provider,
+            env.txDefaults,
+            artifacts,
+        );
+    });
+
+    blockchainTests.resets('setParams()', () => {
+        async function setParamsAndAssertAsync(params: Partial<StakingParams>, from?: string): Promise<void> {
+            const _params = {
+                ...stakingConstants.DEFAULT_PARAMS,
+                ...params,
+            };
+            const receipt = await testContract.setParams.awaitTransactionSuccessAsync(
+                new BigNumber(_params.epochDurationInSeconds),
+                new BigNumber(_params.rewardDelegatedStakeWeight),
+                new BigNumber(_params.minimumPoolStake),
+                new BigNumber(_params.maximumMakersInPool),
+                new BigNumber(_params.cobbDouglasAlphaNumerator),
+                new BigNumber(_params.cobbDouglasAlphaDenominator),
+                _params.wethProxyAddress,
+                _params.ethVaultAddress,
+                _params.rewardVaultAddress,
+                _params.zrxVaultAddress,
+                { from },
+            );
+            // Assert event.
+            expect(receipt.logs.length).to.eq(1);
+            const event = filterLogsToArguments<IStakingEventsParamsSetEventArgs>(receipt.logs, 'ParamsSet')[0];
+            expect(event.epochDurationInSeconds).to.bignumber.eq(_params.epochDurationInSeconds);
+            expect(event.rewardDelegatedStakeWeight).to.bignumber.eq(_params.rewardDelegatedStakeWeight);
+            expect(event.minimumPoolStake).to.bignumber.eq(_params.minimumPoolStake);
+            expect(event.maximumMakersInPool).to.bignumber.eq(_params.maximumMakersInPool);
+            expect(event.cobbDouglasAlphaNumerator).to.bignumber.eq(_params.cobbDouglasAlphaNumerator);
+            expect(event.cobbDouglasAlphaDenominator).to.bignumber.eq(_params.cobbDouglasAlphaDenominator);
+            expect(event.wethProxyAddress).to.eq(_params.wethProxyAddress);
+            expect(event.ethVaultAddress).to.eq(_params.ethVaultAddress);
+            expect(event.rewardVaultAddress).to.eq(_params.rewardVaultAddress);
+            expect(event.zrxVaultAddress).to.eq(_params.zrxVaultAddress);
+            // Assert `getParams()`.
+            const actual = await testContract.getParams.callAsync();
+            expect(actual[0]).to.bignumber.eq(_params.epochDurationInSeconds);
+            expect(actual[1]).to.bignumber.eq(_params.rewardDelegatedStakeWeight);
+            expect(actual[2]).to.bignumber.eq(_params.minimumPoolStake);
+            expect(actual[3]).to.bignumber.eq(_params.maximumMakersInPool);
+            expect(actual[4]).to.bignumber.eq(_params.cobbDouglasAlphaNumerator);
+            expect(actual[5]).to.bignumber.eq(_params.cobbDouglasAlphaDenominator);
+            expect(actual[6]).to.eq(_params.wethProxyAddress);
+            expect(actual[7]).to.eq(_params.ethVaultAddress);
+            expect(actual[8]).to.eq(_params.rewardVaultAddress);
+            expect(actual[9]).to.eq(_params.zrxVaultAddress);
+        }
+
+        it('throws if not called by owner', async () => {
+            const tx = setParamsAndAssertAsync({}, notOwnerAddress);
+            const expectedError = new OwnableRevertErrors.OnlyOwnerError(notOwnerAddress, ownerAddress);
+            return expect(tx).to.revertWith(expectedError);
+        });
+
+        it('works if called by owner', async () => {
+            return setParamsAndAssertAsync({});
+        });
+
+        describe('rewardDelegatedStakeWeight', () => {
+            it('throws when > PPM_100_PERCENT', async () => {
+                const params = {
+                    rewardDelegatedStakeWeight: constants.PPM_100_PERCENT + 1,
+                };
+                const tx = setParamsAndAssertAsync(params);
+                const expectedError = new StakingRevertErrors.InvalidParamValueError(
+                    StakingRevertErrors.InvalidParamValueErrorCode.InvalidRewardDelegatedStakeWeight,
+                );
+                return expect(tx).to.revertWith(expectedError);
+            });
+        });
+
+        describe('maximumMakersInPool', () => {
+            it('throws when == 0', async () => {
+                const params = {
+                    maximumMakersInPool: constants.ZERO_AMOUNT,
+                };
+                const tx = setParamsAndAssertAsync(params);
+                const expectedError = new StakingRevertErrors.InvalidParamValueError(
+                    StakingRevertErrors.InvalidParamValueErrorCode.InvalidMaximumMakersInPool,
+                );
+                return expect(tx).to.revertWith(expectedError);
+            });
+        });
+
+        describe('cobb-douglas alpha', () => {
+            it('throws with denominator == 0', async () => {
+                const params = {
+                    cobbDouglasAlphaNumerator: 0,
+                    cobbDouglasAlphaDenominator: 0,
+                };
+                const tx = setParamsAndAssertAsync(params);
+                const expectedError = new StakingRevertErrors.InvalidParamValueError(
+                    StakingRevertErrors.InvalidParamValueErrorCode.InvalidCobbDouglasAlpha,
+                );
+                return expect(tx).to.revertWith(expectedError);
+            });
+
+            it('throws with numerator > denominator', async () => {
+                const params = {
+                    cobbDouglasAlphaNumerator: 2,
+                    cobbDouglasAlphaDenominator: 1,
+                };
+                const tx = setParamsAndAssertAsync(params);
+                const expectedError = new StakingRevertErrors.InvalidParamValueError(
+                    StakingRevertErrors.InvalidParamValueErrorCode.InvalidCobbDouglasAlpha,
+                );
+                return expect(tx).to.revertWith(expectedError);
+            });
+
+            it('accepts numerator == denominator', async () => {
+                const params = {
+                    cobbDouglasAlphaNumerator: 1,
+                    cobbDouglasAlphaDenominator: 1,
+                };
+                return setParamsAndAssertAsync(params);
+            });
+
+            it('accepts numerator < denominator', async () => {
+                const params = {
+                    cobbDouglasAlphaNumerator: 1,
+                    cobbDouglasAlphaDenominator: 2,
+                };
+                return setParamsAndAssertAsync(params);
+            });
+
+            it('accepts numerator == 0', async () => {
+                const params = {
+                    cobbDouglasAlphaNumerator: 0,
+                    cobbDouglasAlphaDenominator: 1,
+                };
+                return setParamsAndAssertAsync(params);
+            });
+        });
+    });
+});
+// tslint:enable:no-unnecessary-type-assertion
