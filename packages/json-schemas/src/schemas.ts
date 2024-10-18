cb0f97abd9ed7a5623b9816e2bdfdc5668f7c8bd
xan187 <cloud901memphis@gmail.com>
2024-10-18 02:39:33 -0500

    Initialize dependabot/npm_and_yarn/ethereumjs-tx-1.3.7

---
+++ packages/json-schemas/src/schemas.ts
@@ -0,0 +1,71 @@
+import { addressSchema, hexSchema, numberSchema } from '../schemas/basic_type_schemas';
+import { blockParamSchema, blockRangeSchema } from '../schemas/block_range_schema';
+import { callDataSchema } from '../schemas/call_data_schema';
+import { ecSignatureParameterSchema, ecSignatureSchema } from '../schemas/ec_signature_schema';
+import { indexFilterValuesSchema } from '../schemas/index_filter_values_schema';
+import { orderCancellationRequestsSchema } from '../schemas/order_cancel_schema';
+import { orderFillOrKillRequestsSchema } from '../schemas/order_fill_or_kill_requests_schema';
+import { orderFillRequestsSchema } from '../schemas/order_fill_requests_schema';
+import { orderHashSchema } from '../schemas/order_hash_schema';
+import { orderSchema, signedOrderSchema } from '../schemas/order_schemas';
+import { ordersSchema } from '../schemas/orders_schema';
+import { paginatedCollectionSchema } from '../schemas/paginated_collection_schema';
+import {
+    relayerApiAssetDataPairsResponseSchema,
+    relayerApiAssetDataPairsSchema,
+    relayerApiAssetDataTradeInfoSchema,
+} from '../schemas/relayer_api_asset_pairs_response_schema';
+import { relayerApiErrorResponseSchema } from '../schemas/relayer_api_error_response_schema';
+import { relayerApiFeeRecipientsResponseSchema } from '../schemas/relayer_api_fee_recipients_response_schema';
+import { relayerApiOrderConfigPayloadSchema } from '../schemas/relayer_api_order_config_payload_schema';
+import { relayerApiOrderConfigResponseSchema } from '../schemas/relayer_api_order_config_response_schema';
+import { relayerApiOrderSchema } from '../schemas/relayer_api_order_schema';
+import { relayerApiOrderbookResponseSchema } from '../schemas/relayer_api_orderbook_response_schema';
+import {
+    relayerApiOrdersChannelSubscribePayload,
+    relayerApiOrdersChannelSubscribeSchema,
+} from '../schemas/relayer_api_orders_channel_subscribe_schema';
+import { relayerApiOrdersChannelUpdateSchema } from '../schemas/relayer_api_orders_channel_update_response_schema';
+import { relayerApiOrdersResponseSchema } from '../schemas/relayer_api_orders_response_schema';
+import { relayerApiOrdersSchema } from '../schemas/relayer_api_orders_schema';
+import { signedOrdersSchema } from '../schemas/signed_orders_schema';
+import { tokenSchema } from '../schemas/token_schema';
+import { jsNumber, txDataSchema } from '../schemas/tx_data_schema';
+
+export const schemas = {
+    numberSchema,
+    addressSchema,
+    callDataSchema,
+    hexSchema,
+    ecSignatureParameterSchema,
+    ecSignatureSchema,
+    indexFilterValuesSchema,
+    orderCancellationRequestsSchema,
+    orderFillOrKillRequestsSchema,
+    orderFillRequestsSchema,
+    orderHashSchema,
+    orderSchema,
+    signedOrderSchema,
+    signedOrdersSchema,
+    ordersSchema,
+    blockParamSchema,
+    blockRangeSchema,
+    tokenSchema,
+    jsNumber,
+    txDataSchema,
+    paginatedCollectionSchema,
+    relayerApiErrorResponseSchema,
+    relayerApiFeeRecipientsResponseSchema,
+    relayerApiOrderSchema,
+    relayerApiOrdersSchema,
+    relayerApiOrderConfigPayloadSchema,
+    relayerApiOrderConfigResponseSchema,
+    relayerApiOrderbookResponseSchema,
+    relayerApiAssetDataPairsResponseSchema,
+    relayerApiAssetDataTradeInfoSchema,
+    relayerApiOrdersChannelSubscribeSchema,
+    relayerApiOrdersChannelSubscribePayload,
+    relayerApiOrdersChannelUpdateSchema,
+    relayerApiOrdersResponseSchema,
+    relayerApiAssetDataPairsSchema,
+};
---
+++ packages/order-utils/src/order_state_utils.ts
@@ -0,0 +1,287 @@
+import {
+    ExchangeContractErrs,
+    OrderRelevantState,
+    OrderState,
+    OrderStateInvalid,
+    OrderStateValid,
+    SignedOrder,
+} from '@0xproject/types';
+import { BigNumber } from '@0xproject/utils';
+
+import { AbstractBalanceAndProxyAllowanceFetcher } from './abstract/abstract_balance_and_proxy_allowance_fetcher';
+import { AbstractOrderFilledCancelledFetcher } from './abstract/abstract_order_filled_cancelled_fetcher';
+import { orderHashUtils } from './order_hash';
+import { OrderValidationUtils } from './order_validation_utils';
+import { RemainingFillableCalculator } from './remaining_fillable_calculator';
+import { utils } from './utils';
+
+interface SidedOrderRelevantState {
+    isMakerSide: boolean;
+    traderBalance: BigNumber;
+    traderProxyAllowance: BigNumber;
+    traderFeeBalance: BigNumber;
+    traderFeeProxyAllowance: BigNumber;
+    filledTakerAssetAmount: BigNumber;
+    remainingFillableAssetAmount: BigNumber;
+    isOrderCancelled: boolean;
+}
+interface OrderValidResult {
+    isValid: true;
+}
+interface OrderInvalidResult {
+    isValid: false;
+    error: ExchangeContractErrs;
+}
+type OrderValidationResult = OrderValidResult | OrderInvalidResult;
+
+export class OrderStateUtils {
+    private readonly _balanceAndProxyAllowanceFetcher: AbstractBalanceAndProxyAllowanceFetcher;
+    private readonly _orderFilledCancelledFetcher: AbstractOrderFilledCancelledFetcher;
+    private static _validateIfOrderIsValid(
+        signedOrder: SignedOrder,
+        sidedOrderRelevantState: SidedOrderRelevantState,
+    ): OrderValidationResult {
+        const isMakerSide = sidedOrderRelevantState.isMakerSide;
+        if (sidedOrderRelevantState.isOrderCancelled) {
+            return { isValid: false, error: ExchangeContractErrs.OrderCancelled };
+        }
+        const availableTakerAssetAmount = signedOrder.takerAssetAmount.minus(
+            sidedOrderRelevantState.filledTakerAssetAmount,
+        );
+        if (availableTakerAssetAmount.eq(0)) {
+            return { isValid: false, error: ExchangeContractErrs.OrderRemainingFillAmountZero };
+        }
+
+        if (sidedOrderRelevantState.traderBalance.eq(0)) {
+            const error = isMakerSide
+                ? ExchangeContractErrs.InsufficientMakerBalance
+                : ExchangeContractErrs.InsufficientTakerBalance;
+            return { isValid: false, error };
+        }
+        if (sidedOrderRelevantState.traderProxyAllowance.eq(0)) {
+            const error = isMakerSide
+                ? ExchangeContractErrs.InsufficientMakerAllowance
+                : ExchangeContractErrs.InsufficientTakerAllowance;
+            return { isValid: false, error };
+        }
+        if (!signedOrder.makerFee.eq(0)) {
+            if (sidedOrderRelevantState.traderFeeBalance.eq(0)) {
+                const error = isMakerSide
+                    ? ExchangeContractErrs.InsufficientMakerFeeBalance
+                    : ExchangeContractErrs.InsufficientTakerFeeBalance;
+                return { isValid: false, error };
+            }
+            if (sidedOrderRelevantState.traderFeeProxyAllowance.eq(0)) {
+                const error = isMakerSide
+                    ? ExchangeContractErrs.InsufficientMakerFeeAllowance
+                    : ExchangeContractErrs.InsufficientTakerFeeAllowance;
+                return { isValid: false, error };
+            }
+        }
+        const remainingTakerAssetAmount = signedOrder.takerAssetAmount.minus(
+            sidedOrderRelevantState.filledTakerAssetAmount,
+        );
+        const isRoundingError = OrderValidationUtils.isRoundingError(
+            remainingTakerAssetAmount,
+            signedOrder.takerAssetAmount,
+            signedOrder.makerAssetAmount,
+        );
+        if (isRoundingError) {
+            return { isValid: false, error: ExchangeContractErrs.OrderFillRoundingError };
+        }
+        return { isValid: true };
+    }
+    constructor(
+        balanceAndProxyAllowanceFetcher: AbstractBalanceAndProxyAllowanceFetcher,
+        orderFilledCancelledFetcher: AbstractOrderFilledCancelledFetcher,
+    ) {
+        this._balanceAndProxyAllowanceFetcher = balanceAndProxyAllowanceFetcher;
+        this._orderFilledCancelledFetcher = orderFilledCancelledFetcher;
+    }
+    public async getOpenOrderStateAsync(signedOrder: SignedOrder): Promise<OrderState> {
+        const orderRelevantState = await this.getOpenOrderRelevantStateAsync(signedOrder);
+        const orderHash = orderHashUtils.getOrderHashHex(signedOrder);
+        const isOrderCancelled = await this._orderFilledCancelledFetcher.isOrderCancelledAsync(orderHash);
+        const sidedOrderRelevantState = {
+            isMakerSide: true,
+            traderBalance: orderRelevantState.makerBalance,
+            traderProxyAllowance: orderRelevantState.makerProxyAllowance,
+            traderFeeBalance: orderRelevantState.makerFeeBalance,
+            traderFeeProxyAllowance: orderRelevantState.makerFeeProxyAllowance,
+            filledTakerAssetAmount: orderRelevantState.filledTakerAssetAmount,
+            remainingFillableAssetAmount: orderRelevantState.remainingFillableMakerAssetAmount,
+            isOrderCancelled,
+        };
+        const orderValidationResult = OrderStateUtils._validateIfOrderIsValid(signedOrder, sidedOrderRelevantState);
+        if (orderValidationResult.isValid) {
+            const orderState: OrderStateValid = {
+                isValid: true,
+                orderHash,
+                orderRelevantState,
+            };
+            return orderState;
+        } else {
+            const orderState: OrderStateInvalid = {
+                isValid: false,
+                orderHash,
+                error: orderValidationResult.error,
+            };
+            return orderState;
+        }
+    }
+    public async getOpenOrderRelevantStateAsync(signedOrder: SignedOrder): Promise<OrderRelevantState> {
+        const isMaker = true;
+        const sidedOrderRelevantState = await this._getSidedOrderRelevantStateAsync(
+            isMaker,
+            signedOrder,
+            signedOrder.takerAddress,
+        );
+        const remainingFillableTakerAssetAmount = sidedOrderRelevantState.remainingFillableAssetAmount
+            .times(signedOrder.takerAssetAmount)
+            .dividedToIntegerBy(signedOrder.makerAssetAmount);
+
+        const orderRelevantState = {
+            makerBalance: sidedOrderRelevantState.traderBalance,
+            makerProxyAllowance: sidedOrderRelevantState.traderProxyAllowance,
+            makerFeeBalance: sidedOrderRelevantState.traderFeeBalance,
+            makerFeeProxyAllowance: sidedOrderRelevantState.traderFeeProxyAllowance,
+            filledTakerAssetAmount: sidedOrderRelevantState.filledTakerAssetAmount,
+            remainingFillableMakerAssetAmount: sidedOrderRelevantState.remainingFillableAssetAmount,
+            remainingFillableTakerAssetAmount,
+        };
+        return orderRelevantState;
+    }
+    public async getMaxFillableTakerAssetAmountAsync(
+        signedOrder: SignedOrder,
+        takerAddress: string,
+    ): Promise<BigNumber> {
+        // Get max fillable amount for an order, considering the makers ability to fill
+        let isMaker = true;
+        const orderRelevantMakerState = await this._getSidedOrderRelevantStateAsync(
+            isMaker,
+            signedOrder,
+            signedOrder.takerAddress,
+        );
+        const remainingFillableTakerAssetAmountGivenMakersStatus = signedOrder.makerAssetAmount.eq(0)
+            ? new BigNumber(0)
+            : utils.getPartialAmount(
+                  orderRelevantMakerState.remainingFillableAssetAmount,
+                  signedOrder.makerAssetAmount,
+                  signedOrder.takerAssetAmount,
+              );
+
+        // Get max fillable amount for an order, considering the takers ability to fill
+        isMaker = false;
+        const orderRelevantTakerState = await this._getSidedOrderRelevantStateAsync(isMaker, signedOrder, takerAddress);
+        const remainingFillableTakerAssetAmountGivenTakersStatus = orderRelevantTakerState.remainingFillableAssetAmount;
+
+        // The min of these two in the actualy max fillable by either party
+        const fillableTakerAssetAmount = BigNumber.min([
+            remainingFillableTakerAssetAmountGivenMakersStatus,
+            remainingFillableTakerAssetAmountGivenTakersStatus,
+        ]);
+
+        return fillableTakerAssetAmount;
+    }
+    public async getMaxFillableTakerAssetAmountForFailingOrderAsync(
+        signedOrder: SignedOrder,
+        takerAddress: string,
+    ): Promise<BigNumber> {
+        // Get min of taker balance & allowance
+        const takerAssetBalanceOfTaker = await this._balanceAndProxyAllowanceFetcher.getBalanceAsync(
+            signedOrder.takerAssetData,
+            takerAddress,
+        );
+        const takerAssetAllowanceOfTaker = await this._balanceAndProxyAllowanceFetcher.getProxyAllowanceAsync(
+            signedOrder.takerAssetData,
+            takerAddress,
+        );
+        const minTakerAssetAmount = BigNumber.min([takerAssetBalanceOfTaker, takerAssetAllowanceOfTaker]);
+
+        // get remainingFillAmount
+        const orderHash = orderHashUtils.getOrderHashHex(signedOrder);
+        const filledTakerAssetAmount = await this._orderFilledCancelledFetcher.getFilledTakerAmountAsync(orderHash);
+        const remainingFillTakerAssetAmount = signedOrder.takerAssetAmount.minus(filledTakerAssetAmount);
+
+        if (minTakerAssetAmount.gte(remainingFillTakerAssetAmount)) {
+            return remainingFillTakerAssetAmount;
+        } else {
+            return minTakerAssetAmount;
+        }
+    }
+    private async _getSidedOrderRelevantStateAsync(
+        isMakerSide: boolean,
+        signedOrder: SignedOrder,
+        takerAddress: string,
+    ): Promise<SidedOrderRelevantState> {
+        let traderAddress;
+        let assetData;
+        let assetAmount;
+        let feeAmount;
+        if (isMakerSide) {
+            traderAddress = signedOrder.makerAddress;
+            assetData = signedOrder.makerAssetData;
+            assetAmount = signedOrder.makerAssetAmount;
+            feeAmount = signedOrder.makerFee;
+        } else {
+            traderAddress = takerAddress;
+            assetData = signedOrder.takerAssetData;
+            assetAmount = signedOrder.takerAssetAmount;
+            feeAmount = signedOrder.takerFee;
+        }
+        const zrxAssetData = this._orderFilledCancelledFetcher.getZRXAssetData();
+        const isAssetZRX = assetData === zrxAssetData;
+
+        const traderBalance = await this._balanceAndProxyAllowanceFetcher.getBalanceAsync(assetData, traderAddress);
+        const traderProxyAllowance = await this._balanceAndProxyAllowanceFetcher.getProxyAllowanceAsync(
+            assetData,
+            traderAddress,
+        );
+        const traderFeeBalance = await this._balanceAndProxyAllowanceFetcher.getBalanceAsync(
+            zrxAssetData,
+            traderAddress,
+        );
+        const traderFeeProxyAllowance = await this._balanceAndProxyAllowanceFetcher.getProxyAllowanceAsync(
+            zrxAssetData,
+            traderAddress,
+        );
+
+        const transferrableTraderAssetAmount = BigNumber.min([traderProxyAllowance, traderBalance]);
+        const transferrableFeeAssetAmount = BigNumber.min([traderFeeProxyAllowance, traderFeeBalance]);
+
+        const orderHash = orderHashUtils.getOrderHashHex(signedOrder);
+        const filledTakerAssetAmount = await this._orderFilledCancelledFetcher.getFilledTakerAmountAsync(orderHash);
+        const totalMakerAssetAmount = signedOrder.makerAssetAmount;
+        const totalTakerAssetAmount = signedOrder.takerAssetAmount;
+        const isOrderCancelled = await this._orderFilledCancelledFetcher.isOrderCancelledAsync(orderHash);
+        const remainingTakerAssetAmount = isOrderCancelled
+            ? new BigNumber(0)
+            : totalTakerAssetAmount.minus(filledTakerAssetAmount);
+        const remainingMakerAssetAmount = remainingTakerAssetAmount.eq(0)
+            ? new BigNumber(0)
+            : remainingTakerAssetAmount.times(totalMakerAssetAmount).dividedToIntegerBy(totalTakerAssetAmount);
+        const remainingAssetAmount = isMakerSide ? remainingMakerAssetAmount : remainingTakerAssetAmount;
+
+        const remainingFillableCalculator = new RemainingFillableCalculator(
+            feeAmount,
+            assetAmount,
+            isAssetZRX,
+            transferrableTraderAssetAmount,
+            transferrableFeeAssetAmount,
+            remainingAssetAmount,
+        );
+        const remainingFillableAssetAmount = remainingFillableCalculator.computeRemainingFillable();
+
+        const sidedOrderRelevantState = {
+            isMakerSide,
+            traderBalance,
+            traderProxyAllowance,
+            traderFeeBalance,
+            traderFeeProxyAllowance,
+            filledTakerAssetAmount,
+            remainingFillableAssetAmount,
+            isOrderCancelled,
+        };
+        return sidedOrderRelevantState;
+    }
+}
