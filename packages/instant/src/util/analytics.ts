29a9f6a81a785bd1ec1f8b9cdd5c10d9a85beb3a
xan187 <cloud901memphis@gmail.com>
2024-10-18 02:42:58 -0500

    Initialize greenkeeper/npm-run-all-4.1.5

---
+++ packages/instant/src/util/analytics.ts
@@ -0,0 +1,70 @@
+import { EventProperties, heapUtil } from './heap';
+
+let isDisabled = false;
+export const disableAnalytics = (shouldDisableAnalytics: boolean) => {
+    isDisabled = shouldDisableAnalytics;
+};
+export const evaluateIfEnabled = (fnCall: () => void) => {
+    if (isDisabled) {
+        return;
+    }
+    fnCall();
+};
+
+enum EventNames {
+    INSTANT_OPENED = 'Instant - Opened',
+    ACCOUNT_LOCKED = 'Account - Locked',
+    ACCOUNT_READY = 'Account - Ready',
+    ACCOUNT_UNLOCK_REQUESTED = 'Account - Unlock Requested',
+    ACCOUNT_UNLOCK_DENIED = 'Account - Unlock Denied',
+    ACCOUNT_ADDRESS_CHANGED = 'Account - Address Changed',
+}
+const track = (eventName: EventNames, eventProperties: EventProperties = {}): void => {
+    evaluateIfEnabled(() => {
+        heapUtil.evaluateHeapCall(heap => heap.track(eventName, eventProperties));
+    });
+};
+function trackingEventFnWithoutPayload(eventName: EventNames): () => void {
+    return () => {
+        track(eventName);
+    };
+}
+// tslint:disable-next-line:no-unused-variable
+function trackingEventFnWithPayload(eventName: EventNames): (eventProperties: EventProperties) => void {
+    return (eventProperties: EventProperties) => {
+        track(eventName, eventProperties);
+    };
+}
+
+export interface AnalyticsUserOptions {
+    lastKnownEthAddress?: string;
+    ethBalanceInUnitAmount?: string;
+}
+export interface AnalyticsEventOptions {
+    embeddedHost?: string;
+    embeddedUrl?: string;
+    networkId?: number;
+    providerName?: string;
+    gitSha?: string;
+    npmVersion?: string;
+}
+
+export const analytics = {
+    addUserProperties: (properties: AnalyticsUserOptions): void => {
+        evaluateIfEnabled(() => {
+            heapUtil.evaluateHeapCall(heap => heap.addUserProperties(properties));
+        });
+    },
+    addEventProperties: (properties: AnalyticsEventOptions): void => {
+        evaluateIfEnabled(() => {
+            heapUtil.evaluateHeapCall(heap => heap.addEventProperties(properties));
+        });
+    },
+    trackInstantOpened: trackingEventFnWithoutPayload(EventNames.INSTANT_OPENED),
+    trackAccountLocked: trackingEventFnWithoutPayload(EventNames.ACCOUNT_LOCKED),
+    trackAccountReady: (address: string) => trackingEventFnWithPayload(EventNames.ACCOUNT_READY)({ address }),
+    trackAccountUnlockRequested: trackingEventFnWithoutPayload(EventNames.ACCOUNT_UNLOCK_REQUESTED),
+    trackAccountUnlockDenied: trackingEventFnWithoutPayload(EventNames.ACCOUNT_UNLOCK_DENIED),
+    trackAccountAddressChanged: (address: string) =>
+        trackingEventFnWithPayload(EventNames.ACCOUNT_ADDRESS_CHANGED)({ address }),
+};
