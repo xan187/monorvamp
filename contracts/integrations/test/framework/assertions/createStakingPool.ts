276bb251af10ff70ffa3c10c2678410c2a092407
xan187 <cloud901memphis@gmail.com>
2024-10-18 03:19:35 -0500

    Initialize feat/forwarder/multi-affiliate-fees

---
+++ contracts/integrations/test/framework/assertions/createStakingPool.ts
@@ -0,0 +1,60 @@
+import { StakingPoolById, StoredBalance } from '@0x/contracts-staking';
+import { expect } from '@0x/contracts-test-utils';
+import { BigNumber, logUtils } from '@0x/utils';
+import { TxData } from 'ethereum-types';
+
+import { DeploymentManager } from '../deployment_manager';
+
+import { FunctionAssertion, FunctionResult } from './function_assertion';
+
+// tslint:disable:no-unnecessary-type-assertion
+
+/**
+ * Returns a FunctionAssertion for `createStakingPool` which assumes valid input is provided. The
+ * FunctionAssertion checks that the new poolId is one more than the last poolId.
+ */
+/* tslint:disable:no-non-null-assertion */
+export function validCreateStakingPoolAssertion(
+    deployment: DeploymentManager,
+    pools: StakingPoolById,
+): FunctionAssertion<[number, boolean], string, string> {
+    const { stakingWrapper } = deployment.staking;
+
+    return new FunctionAssertion<[number, boolean], string, string>(
+        stakingWrapper.createStakingPool.bind(stakingWrapper),
+        {
+            // Returns the expected ID of th created pool
+            before: async () => {
+                const lastPoolId = await stakingWrapper.lastPoolId().callAsync();
+                // Effectively the last poolId + 1, but as a bytestring
+                return `0x${new BigNumber(lastPoolId)
+                    .plus(1)
+                    .toString(16)
+                    .padStart(64, '0')}`;
+            },
+            after: async (
+                expectedPoolId: string,
+                result: FunctionResult,
+                args: [number, boolean],
+                txData: Partial<TxData>,
+            ) => {
+                const [operatorShare, shouldAddMakerAsOperator] = args;
+
+                logUtils.log(`createStakingPool(${operatorShare}, ${shouldAddMakerAsOperator}) => ${expectedPoolId}`);
+
+                // Checks the logs for the new poolId, verifies that it is as expected
+                const log = result.receipt!.logs[0];
+                const actualPoolId = (log as any).args.poolId;
+                expect(actualPoolId).to.equal(expectedPoolId);
+
+                // Adds the new pool to local state
+                pools[actualPoolId] = {
+                    operator: txData.from!,
+                    operatorShare,
+                    delegatedStake: new StoredBalance(),
+                };
+            },
+        },
+    );
+}
+/* tslint:enable:no-non-null-assertion*/
---
+++ packages/asset-swapper/test/utils/swap_quote.ts
@@ -0,0 +1,57 @@
+import { SignedOrder } from '@0x/types';
+import { BigNumber } from '@0x/utils';
+import * as _ from 'lodash';
+
+import { constants } from '../../src/constants';
+import { MarketOperation, PrunedSignedOrder, SwapQuote } from '../../src/types';
+import { ProtocolFeeUtils } from '../../src/utils/protocol_fee_utils';
+
+export const getFullyFillableSwapQuoteWithNoFeesAsync = async (
+    makerAssetData: string,
+    takerAssetData: string,
+    orders: PrunedSignedOrder[],
+    operation: MarketOperation,
+    gasPrice: BigNumber,
+    protocolFeeUtils: ProtocolFeeUtils,
+): Promise<SwapQuote> => {
+    const makerAssetFillAmount = _.reduce(
+        orders,
+        (a: BigNumber, c: SignedOrder) => a.plus(c.makerAssetAmount),
+        constants.ZERO_AMOUNT,
+    );
+    const totalTakerAssetAmount = _.reduce(
+        orders,
+        (a: BigNumber, c: SignedOrder) => a.plus(c.takerAssetAmount),
+        constants.ZERO_AMOUNT,
+    );
+    const quoteInfo = {
+        makerAssetAmount: makerAssetFillAmount,
+        feeTakerAssetAmount: constants.ZERO_AMOUNT,
+        takerAssetAmount: totalTakerAssetAmount,
+        totalTakerAssetAmount,
+        protocolFeeInWeiAmount: await protocolFeeUtils.calculateWorstCaseProtocolFeeAsync(orders, gasPrice),
+    };
+
+    const quoteBase = {
+        makerAssetData,
+        takerAssetData,
+        orders,
+        gasPrice,
+        bestCaseQuoteInfo: quoteInfo,
+        worstCaseQuoteInfo: quoteInfo,
+    };
+
+    if (operation === MarketOperation.Buy) {
+        return {
+            ...quoteBase,
+            type: MarketOperation.Buy,
+            makerAssetFillAmount,
+        };
+    } else {
+        return {
+            ...quoteBase,
+            type: MarketOperation.Sell,
+            takerAssetFillAmount: totalTakerAssetAmount,
+        };
+    }
+};
